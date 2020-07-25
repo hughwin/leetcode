83968285a9545611e7ede364b507b04ee5dff6a2
hughwin <hugh.winchester@gmail.com>
2020-07-25 10:40:22 +0100

    Added middleNode solution

--- src/Solutions.java
+++ src/Solutions.java
@@ -1005,6 +1005,34 @@ public class Solutions {
         return count;
     }
 
+    // https://leetcode.com/problems/middle-of-the-linked-list/
+    public ListNode middleNode(ListNode head) {
+        ArrayList<ListNode> nodes = new ArrayList<>();
+        while(head != null){
+            nodes.add(head);
+            head = head.next;
+        }
+        return nodes.get(nodes.size() / 2);
+    }
+
+    @Test
+    public void middleNode() {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        ListNode f = new ListNode(6);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        Assert.assertEquals(d, middleNode(a));
+    }
+    
+
+
     @Test
     public void numRookCapturesTest(){
 //        char[][] board = new char[][]

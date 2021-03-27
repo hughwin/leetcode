bfda03cc38419e67336a0d167bee49b9c6eae0e6
Hugh Winchester <hugh.winchester@gmail.com>
2021-03-27 02:27:11 +0000

    Added solution for isUgly

--- src/Solutions.java
+++ src/Solutions.java
@@ -3493,5 +3493,30 @@ public class Solutions {
         Assert.assertFalse(isHappy(2));
     }
 
+    public boolean isUgly(int n) {
+            if(n==0)return false;
+            while(n!=1)
+            {
+                if(n%2==0)
+                    n=n/2;
+                else if(n%3==0)
+                    n=n/3;
+                else if(n%5==0)
+                    n=n/5;
+                else
+                    return false;
+
+            }
+            return true;
+    }
+
+    @Test
+    public void isUglyTest(){
+        Assert.assertTrue(isUgly(6));
+        Assert.assertTrue(isUgly(8));
+        Assert.assertFalse(isUgly(14));
+        Assert.assertFalse(isUgly(-2147483648));
+    }
+
 }
 

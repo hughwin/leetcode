6f8abd05b1572e2fd7228aea78acdd6bc49e9250
hughwin <hugh.winchester@gmail.com>
2021-05-19 11:16:25 +0100

    Added solution for arrayPairSum

--- src/Solutions.java
+++ src/Solutions.java
@@ -4041,5 +4041,21 @@ public class Solutions {
 
         Assert.assertEquals(headTest, sortList(head));
     }
+
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    @Test
+    public void arrayPairSumTest() {
+        Assert.assertEquals(4, arrayPairSum(new int[]{1, 4, 3, 2}));
+        Assert.assertEquals(9, arrayPairSum(new int[]{6, 2, 6, 5, 1, 2}));
+
+    }
 }
 

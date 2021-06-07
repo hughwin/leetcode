6b63ac9dfcc3eec5d076843befcdc8bc1769cd8a
hughwin <hugh.winchester@gmail.com>
2021-06-07 10:24:21 +0100

    Added solution for intersection

--- src/Solutions.java
+++ src/Solutions.java
@@ -4424,7 +4424,27 @@ public class Solutions {
     public void xorOperationTest() {
         Assert.assertEquals(8, xorOperation(5, 0));
         Assert.assertEquals(8, xorOperation(4, 3));
+    }
 
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> hashSet1 = new HashSet<>();
+        for (int i : nums1) hashSet1.add(i);
+
+        HashSet<Integer> hashSet2 = new HashSet<>();
+        for (int i : nums2) hashSet2.add(i);
+        hashSet1.retainAll(hashSet2);
+        int[] res = new int[hashSet1.size()];
+        int index = 0;
+        for (Integer i : hashSet1) {
+            res[index++] = i;
+        }
+        return res;
+    }
+
+    @Test
+    public void intersectionTest() {
+        Assert.assertArrayEquals(new int[]{2}, intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
+        Assert.assertArrayEquals(new int[]{4, 9}, intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}));
     }
 
 

3ed6ae48666a6527a14f08b3839e0e2460914cf8
hughwin <hugh.winchester@gmail.com>
2021-06-09 10:47:13 +0100

    Added solution for countGoodTriplets

--- src/Solutions.java
+++ src/Solutions.java
@@ -4451,6 +4451,24 @@ public class Solutions {
         Assert.assertArrayEquals(new int[]{4, 9}, intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}));
     }
 
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int res = 0, n = arr.length;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                if (Math.abs(arr[i] - arr[j]) > a) continue;
+                for (int k = j + 1; k < n; k++) {
+                    if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c) res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void countGoodTripletsTest() {
+        Assert.assertEquals(4, countGoodTriplets(new int[]{3, 0, 1, 1, 9, 7}, 7, 2, 3));
+    }
+
 
 }
 

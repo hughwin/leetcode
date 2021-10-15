6b7e77706e8712f3171ccf3017d64d6b2b5b3da8
Hugh Winchester <hugh.winchester@gmail.com>
2021-10-15 10:56:45 +0100

    Added solution for maxSubArray

--- src/Solutions.java
+++ src/Solutions.java
@@ -5082,6 +5082,21 @@ public class Solutions {
         Assert.assertFalse(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2));
     }
 
+    public static int maxSubArray(int[] A) {
+        int maxSoFar=A[0], maxEndingHere=A[0];
+        for (int i=1;i<A.length;++i){
+            maxEndingHere= Math.max(maxEndingHere+A[i],A[i]);
+            maxSoFar=Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+
+    @Test
+    public void maxSubArray(){
+        Assert.assertEquals(6, maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        Assert.assertEquals(-1, maxSubArray(new int[]{-1}));
+        Assert.assertEquals(-1, maxSubArray(new int[]{-2,-1}));
+    }
 }
 
 

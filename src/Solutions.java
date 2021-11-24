c99f12d7da61724ec75a8451969903025e149265
Hugh Winchester <hugh.winchester@gmail.com>
2021-11-24 12:04:24 +0000

    Added solution for timeRequiredToBuy

--- src/Solutions.java
+++ src/Solutions.java
@@ -5863,6 +5863,26 @@ public class Solutions {
         Assert.assertEquals("jkab", freqAlphabets("10#11#12"));
     }
 
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count = 0;
+        while(tickets[k] > 0){
+            for(int i = 0; i < tickets.length; i++){
+                if(tickets[k] == 0) break;
+                else if (tickets[i] > 0){
+                    tickets[i] -= 1;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    @Test
+    public void timeRequiredToBuyTest () {
+        Assert.assertEquals(154, timeRequiredToBuy(new int[]{84,49,5,24,70,77,87,8}, 3));
+        Assert.assertEquals(6, timeRequiredToBuy(new int[]{2,3,2}, 2));
+        Assert.assertEquals(8, timeRequiredToBuy(new int[]{5,1,1,1}, 0));
+    }
 
 }
 

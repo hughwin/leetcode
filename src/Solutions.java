d7b7df51587a4eced4423206184ef10db28b19b3
Hugh Winchester <hugh.winchester@gmail.com>
2021-12-30 18:24:37 +0000

    Added a solution for dominantIndex

--- src/Solutions.java
+++ src/Solutions.java
@@ -6311,6 +6311,24 @@ public class Solutions {
         Assert.assertEquals("00", findDifferentBinaryString(new String[]{"01", "10"}));
     }
 
+    public int dominantIndex(int[] nums) {
+        int max = -1, index = -1, second = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                second = max;
+                max = nums[i];
+                index = i;
+            } else if (nums[i] > second)
+                second = nums[i];
+        }
+        return second * 2 <= max ? index : -1;
+    }
+
+    @Test
+    public void dominantIndexTest() {
+        Assert.assertEquals(1, dominantIndex(new int[]{3, 6, 1, 0}));
+    }
+
 }
 
 

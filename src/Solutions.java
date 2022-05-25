a0d70da8d7e2a987f9426c7ecf3c8d37e186f797
Hugh Winchester <hugh.winchester@gmail.com>
2022-05-25 20:42:29 +0100

    Added solution for minDepth

--- src/Solutions.java
+++ src/Solutions.java
@@ -6450,6 +6450,15 @@ public class Solutions {
         Assert.assertEquals(4, maximumDifference(input));
     }
 
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        if (root.left != null && root.right != null)
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        else
+            return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
 }
 
 

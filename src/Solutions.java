625ed38d5287ae7b1c7636a64bdbbf4e7608253d
hughwin <hugh.winchester@gmail.com>
2022-09-27 17:47:22 +0100

    Added  solution for count pairs

--- src/Solutions.java
+++ src/Solutions.java
@@ -5064,6 +5064,22 @@ public class Solutions {
         Assert.assertEquals(4, sum(2, 2));
     }
 
+    public int countPairs(int[] nums, int k) {
+        int res = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && (i * j) % k == 0) res++;
+            }
+        }
+
+        return res;
+    }
+
+    @Test
+    public void countPairsTest() {
+        Assert.assertEquals(4, countPairs(new int[]{3, 1, 2, 2, 2, 1, 3}, 2));
+    }
+
 }
 
 
